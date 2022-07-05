8dc770e9dc1db5beac42e33dcc1b0252f12b440d
 zulya06 <kurbanova.z1990@mail.ru>
2022-07-05 20:54:02 +0300

    creat task4

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿//программа принимает на вход число N, а на выходе показывает все четные числа от 1 до N
+
+int number;
+
+
+Console.WriteLine("input number");
+
+number=Convert.ToInt32 (Console.ReadLine());
+
+for (int i = 2; i<=number; i++) 
+{
+    if(i%2 == 0) 
+    {
+        Console.Write("{0} ", i);
+    }
+}
+
+
+
